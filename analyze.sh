11030c59410f90566cbbfcdad68886c7e4241490
高海峰 <21373512@buaa.edu.cn>
2023-03-06 20:01:37 +0800

    0.0.2

--- analyze.sh
+++ analyze.sh
@@ -2,22 +2,21 @@
 
 if [ $# -eq 1 ];
 then
-	grep -E 'WARN|ERROR' $1 > bug.txt
     # Your code here. (1/4)i
-
+	grep -E 'WARN|ERROR' $1 > bug.txt
 else
     case $2 in
     "--latest")
         # Your code here. (2/4)
-
+	tail -n 5 $s1
     ;;
     "--find")
         # Your code here. (3/4)
-
+	grep $s3 $1 > $3.txt
     ;;
     "--diff")
         # Your code here. (4/4)
-
+	
     ;;
     esac
 fi
