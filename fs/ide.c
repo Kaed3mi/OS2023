9320e1847aef6620f8bd30c68e89c5aa66921692
高海峰 <21373512@buaa.edu.cn>
2023-05-14 21:56:32 +0800

    Lab5 first part

--- fs/ide.c
+++ fs/ide.c
@@ -32,7 +32,22 @@ void ide_read(u_int diskno, u_int secno, void *dst, u_int nsecs) {
 	for (u_int off = 0; begin + off < end; off += BY2SECT) {
 		uint32_t temp = diskno;
 		/* Exercise 5.3: Your code here. (1/2) */
-
+    	uint32_t status = 0;
+		uint32_t cur_offset = begin + off;
+		uint32_t read_flag = DEV_DISK_OPERATION_READ;
+		//diskno
+      	panic_on(syscall_write_dev(&temp, DEV_DISK_ADDRESS + DEV_DISK_ID, sizeof(temp)));
+		//disk offset
+      	panic_on(syscall_write_dev(&cur_offset, DEV_DISK_ADDRESS + DEV_DISK_OFFSET, sizeof(cur_offset)));
+		//0 stands for read
+      	panic_on(syscall_write_dev(&read_flag, DEV_DISK_ADDRESS + DEV_DISK_START_OPERATION, sizeof(read_flag)));
+		//get return value
+      	panic_on(syscall_read_dev(&status, DEV_DISK_ADDRESS + DEV_DISK_STATUS, sizeof(status)));
+    	if (status == 0) {
+      		user_panic("disk read error 5\n");
+    	}
+		//从缓冲区读出要读的数据
+      	panic_on(syscall_read_dev(dst + off, DEV_DISK_ADDRESS + DEV_DISK_BUFFER, DEV_DISK_BUFFER_LEN));
 	}
 }
 
@@ -59,6 +74,21 @@ void ide_write(u_int diskno, u_int secno, void *src, u_int nsecs) {
 	for (u_int off = 0; begin + off < end; off += BY2SECT) {
 		uint32_t temp = diskno;
 		/* Exercise 5.3: Your code here. (2/2) */
-
+		uint32_t status = 0;
+		uint32_t cur_offset = begin + off;
+		uint32_t write_flag = DEV_DISK_OPERATION_WRITE;
+		//disno
+      	panic_on(syscall_write_dev(&temp, DEV_DISK_ADDRESS + DEV_DISK_ID, sizeof(temp)));
+		//disk offset
+      	panic_on(syscall_write_dev(&cur_offset, DEV_DISK_ADDRESS + DEV_DISK_OFFSET, sizeof(cur_offset)));
+		//buffer
+     	panic_on(syscall_write_dev(src + off, DEV_DISK_ADDRESS + DEV_DISK_BUFFER, BY2SECT));
+		//1 stands for write
+      	panic_on(syscall_write_dev(&write_flag, DEV_DISK_ADDRESS + DEV_DISK_START_OPERATION, sizeof(write_flag)));
+		//get return value
+      	panic_on(syscall_read_dev(&status, DEV_DISK_ADDRESS + DEV_DISK_STATUS, sizeof(status)));
+    	if (status == 0) {
+      		user_panic("disk read error 6\n");
+    	}
 	}
 }
--- kern/syscall_all.c
+++ kern/syscall_all.c
@@ -452,8 +452,16 @@ int sys_cgetc(void) {
  */
 int sys_write_dev(u_int va, u_int pa, u_int len) {
 	/* Exercise 5.1: Your code here. (1/2) */
-
-	return 0;
+	if (is_illegal_va_range(va, len)) {
+    	return -E_INVAL;
+  	}
+  	if (!((pa >= 0x10000000 && (pa + len) <= 0x10000020) ||
+          (pa >= 0x13000000 && (pa + len) <= 0x13004200) ||
+          (pa >= 0x15000000 && (pa + len) <= 0x15000200))) {
+    	return -E_INVAL;
+  	}
+	memcpy((pa + KSEG1), va, len);
+  	return 0;
 }
 
 /* Overview:
@@ -469,7 +477,15 @@ int sys_write_dev(u_int va, u_int pa, u_int len) {
  */
 int sys_read_dev(u_int va, u_int pa, u_int len) {
 	/* Exercise 5.1: Your code here. (2/2) */
-
+	if (is_illegal_va_range(va, len)) {
+    	return -E_INVAL;
+  	}
+  	if (!((pa >= 0x10000000 && (pa + len) <= 0x10000020) ||
+          (pa >= 0x13000000 && (pa + len) <= 0x13004200) ||
+          (pa >= 0x15000000 && (pa + len) <= 0x15000200))) {
+    	return -E_INVAL;
+  	}
+	memcpy(va, (pa + KSEG1), len);
 	return 0;
 }
 
