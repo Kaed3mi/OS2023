a76240c08ef4a9ed658c159c3590448a984b4600
高海峰 <21373512@buaa.edu.cn>
2023-05-15 01:17:51 +0800

    part1优化代码

--- fs/ide.c
+++ fs/ide.c
@@ -30,21 +30,21 @@ void ide_read(u_int diskno, u_int secno, void *dst, u_int nsecs) {
 	u_int end = begin + nsecs * BY2SECT;
 
 	for (u_int off = 0; begin + off < end; off += BY2SECT) {
-		uint32_t temp = diskno;
 		/* Exercise 5.3: Your code here. (1/2) */
-    	uint32_t status = 0;
-		uint32_t cur_offset = begin + off;
-		uint32_t read_flag = DEV_DISK_OPERATION_READ;
 		//diskno
+		uint32_t temp = diskno;
       	panic_on(syscall_write_dev(&temp, DEV_DISK_ADDRESS + DEV_DISK_ID, sizeof(temp)));
 		//disk offset
-      	panic_on(syscall_write_dev(&cur_offset, DEV_DISK_ADDRESS + DEV_DISK_OFFSET, sizeof(cur_offset)));
+		temp = begin + off;
+      	panic_on(syscall_write_dev(&temp, DEV_DISK_ADDRESS + DEV_DISK_OFFSET, sizeof(temp)));
 		//0 stands for read
-      	panic_on(syscall_write_dev(&read_flag, DEV_DISK_ADDRESS + DEV_DISK_START_OPERATION, sizeof(read_flag)));
+		temp = DEV_DISK_OPERATION_READ;
+      	panic_on(syscall_write_dev(&temp, DEV_DISK_ADDRESS + DEV_DISK_START_OPERATION, sizeof(temp)));
 		//get return value
-      	panic_on(syscall_read_dev(&status, DEV_DISK_ADDRESS + DEV_DISK_STATUS, sizeof(status)));
-    	if (status == 0) {
-      		user_panic("disk read error 5\n");
+
+      	panic_on(syscall_read_dev(&temp, DEV_DISK_ADDRESS + DEV_DISK_STATUS, sizeof(temp)));
+    	if (temp == 0) {
+      		user_panic("return value equals 0\n");
     	}
 		//从缓冲区读出要读的数据
       	panic_on(syscall_read_dev(dst + off, DEV_DISK_ADDRESS + DEV_DISK_BUFFER, DEV_DISK_BUFFER_LEN));
@@ -72,22 +72,23 @@ void ide_write(u_int diskno, u_int secno, void *src, u_int nsecs) {
 	u_int end = begin + nsecs * BY2SECT;
 
 	for (u_int off = 0; begin + off < end; off += BY2SECT) {
-		uint32_t temp = diskno;
 		/* Exercise 5.3: Your code here. (2/2) */
-		uint32_t status = 0;
-		uint32_t cur_offset = begin + off;
-		uint32_t write_flag = DEV_DISK_OPERATION_WRITE;
 		//disno
+		uint32_t temp = diskno;
       	panic_on(syscall_write_dev(&temp, DEV_DISK_ADDRESS + DEV_DISK_ID, sizeof(temp)));
 		//disk offset
-      	panic_on(syscall_write_dev(&cur_offset, DEV_DISK_ADDRESS + DEV_DISK_OFFSET, sizeof(cur_offset)));
+		temp = begin + off;
+      	panic_on(syscall_write_dev(&temp, DEV_DISK_ADDRESS + DEV_DISK_OFFSET, sizeof(temp)));
 		//buffer
+
      	panic_on(syscall_write_dev(src + off, DEV_DISK_ADDRESS + DEV_DISK_BUFFER, BY2SECT));
 		//1 stands for write
-      	panic_on(syscall_write_dev(&write_flag, DEV_DISK_ADDRESS + DEV_DISK_START_OPERATION, sizeof(write_flag)));
+		temp = DEV_DISK_OPERATION_WRITE;
+      	panic_on(syscall_write_dev(&temp, DEV_DISK_ADDRESS + DEV_DISK_START_OPERATION, sizeof(temp)));
 		//get return value
-      	panic_on(syscall_read_dev(&status, DEV_DISK_ADDRESS + DEV_DISK_STATUS, sizeof(status)));
-    	if (status == 0) {
+		
+      	panic_on(syscall_read_dev(&temp, DEV_DISK_ADDRESS + DEV_DISK_STATUS, sizeof(temp)));
+    	if (temp == 0) {
       		user_panic("disk read error 6\n");
     	}
 	}
