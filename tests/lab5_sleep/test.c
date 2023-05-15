6643b499b1452e1d2079f770f1a1b7a342c6ada4
高海峰 <21373512@buaa.edu.cn>
2023-05-15 19:48:14 +0800

    654

--- tests/lab5_sleep/test.c
+++ tests/lab5_sleep/test.c
@@ -2,10 +2,14 @@
 
 int main() {
 	u_int us = 1000 * 1000;
-	u_int before = get_time(&us);
-	//usleep(us);
-	u_int after = get_time(&us);
-	debugf("%d:%d\n", before, us);
-	debugf("my : %d : %d\n", us, us / 1000000);
+	u_int now_time, entry_time = get_time(NULL) % 100;
+	debugf("entry_time : %d\n", entry_time);
+	while (1) {
+		now_time = get_time(NULL) % 100;
+		if(now_time * 1000000 >= entry_time * 1000000 + us) {
+			break;
+		}
+	}
+	debugf("now_time : %d\n", now_time);
 	return 0;
 }
